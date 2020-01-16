50885fb1f9dd448a7da3a686a29ba5218d897af9
Jen Hoang <jenhoang@berkeley.edu>
2020-01-15 20:44:28 -0800

    edit type

--- js/typing.js
+++ js/typing.js
@@ -1,32 +1,21 @@
-let instance = new TypeIt('#typing', {
+let instance = new TypeIt('#ttm', {
   speed: 90,
   lifelike: false,
   cursor: true,
   waitUntilVisible: true,
   afterComplete: function(instance){
     instance.destroy();
-    instance = new TypeIt('#ttm', {
+    new TypeIt('#ttm2', {
+      strings: ["Computer Science.", "art and animation.", 
+      "inclusive storytelling.", "environmentalism.", "collaborative learning."],
       speed: 90,
       lifelike: false,
       cursor: true,
-      autoStart: false,
-      afterComplete: function(instance){
-        instance.destroy();
-        new TypeIt('#ttm2', {
-          strings: ["Computer Science.", "art and animation.", 
-          "inclusive storytelling.", "environmentalism.", "collaborative learning."],
-          speed: 90,
-          lifelike: false,
-          cursor: true,
-          breakLines: false,
-          autoStart: true,
-          loop: true
-        });
-      }
-    })
-    .break()
-    .type('Talk to me about ');    
+      breakLines: false,
+      autoStart: true,
+      loop: true
+    });
   }
 })
-.type('Hi, I\'m Jen!')
 .pause(1000)
+.type('Talk to me about ');
