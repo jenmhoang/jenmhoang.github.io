ae1e9ca81e6ae96b9cfb5378a51d1c362421baf8
Jen Hoang <jenhoang@berkeley.edu>
2020-04-28 17:54:38 -0700

    update anim

--- js/horizAnim.js
+++ js/horizAnim.js
@@ -1,27 +1,24 @@
-function open(section, tab){
-  $(".projNav").animate({width: '0'});
-  $(".button").hide(1000);
-  $(".info").animate({width: '70%'});
-
-  $(tab).fadeIn(1000);
-  $(section).css("display", "flex");
-  $(section).fadeIn();
-}
-
-function close(){
-  $(".projNav").animate({width: '70%'});
-  $(".button").fadeIn(1000);
-  $(".info").animate({width: '0'});
-  
-  var ids = ["#d1", "#d2", "#d3", "#d4"];
-  $.each(ids, function(i, v) {
-      $(v).fadeOut();
-  })
-  $("#mainPic").fadeIn();
-  $("#togglePic").hide();
-};
-
 $(document).ready(function(){
+  function open(section, tab){
+    $(".section").fadeOut(800);
+    $(".pic").animate({left: '-=70%'}, 1000);
+  
+    $(tab).fadeIn(0);
+    $(section).fadeIn(2000);
+    $(section).css("display", "flex");
+  }
+  
+  function close(){
+    $(".section").fadeIn(1000);
+    $(".pic").animate({left: '+=70%'}, 1000);
+    
+    var ids = ["#d1", "#d2", "#d3", "#d4"];
+    $.each(ids, function(i, v) {
+        $(v).fadeOut();
+    })
+    $("#mainPic").fadeIn();
+    $("#togglePic").hide();
+  };
   var btns = ["#b1", "#b2", "#b3", "#b4"];
   var tab = ["#d1-t1", "#d2-t1", "#d3-t3", "#d4-t4"];
   var sec = ["#d1", "#d2", "#d3", "#d4"];
