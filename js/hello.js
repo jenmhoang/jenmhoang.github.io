1229c830093e176a68d8370c3672d8b04fc78dab
jenmhoang <jenhoang@berkeley.edu>
2020-05-27 00:11:57 -0700

    update wip

--- js/hello.js
+++
@@ -1,9 +0,0 @@
-var hello = new Array("H E L L O", "B O N J O U R", "H O L A", "C I A O", "A L O H A");
-var i = 0;
-setInterval(function(){				
-	document.getElementById("output").innerHTML = hello[i];
-	if (i<4)
-		i++;
-	else
-		i = 0;
-}, 1700);
