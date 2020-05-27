149719a55c4e690bf40430290e4962d8b4879dff
jenmhoang <jenhoang@berkeley.edu>
2020-05-26 18:10:29 -0700

    update tab toggle

--- js/horizAnim.js
+++ js/horizAnim.js
@@ -1,36 +1,36 @@
-$(document).ready(function(){
-  function open(section, tab){
-    $(".pic").animate({right: '70%'}, 1000);
-    $(".info").css("right", '0%');
+$(document).ready(function () {
+  function open(section, tab) {
+    $(".pic").animate({ right: "70%" }, 1000);
+    $(".info").css("right", "0%");
     $(".section").fadeOut();
-  
+
     $(section).fadeIn(2000);
   }
-  
-  function close(){
-    $(".pic").animate({right: '0%'}, 500);
-    $(".info").animate({right: '-70%'}, 500);
+
+  function close() {
+    $(".pic").animate({ right: "0%" }, 500);
+    $(".info").animate({ right: "-70%" }, 500);
     $(".section").fadeIn(500);
-    
+
     var ids = ["#d1", "#d2", "#d3", "#d4"];
-    $.each(ids, function(i, v) {
+    $.each(ids, function (i, v) {
       $(v).fadeOut();
-  })
+    });
     $("#mainPic").fadeIn();
     $("#togglePic").hide();
-  };
+  }
   var btns = ["#b1", "#b2", "#b3", "#b4"];
   var tab = ["#d1-t1", "#d2-t1", "#d3-t3", "#d4-t4"];
   var sec = ["#d1", "#d2", "#d3", "#d4"];
   var cl = [".pic", ".close", ".return"];
-  $.each(btns, function(i, v) {
-    $(v).click(function() {
-        open(sec[i], tab[i]);
-    })
-  })
-  $.each(cl, function(i, v) {
-    $(v).click(function() {
-        close();
+  $.each(btns, function (i, v) {
+    $(v).click(function () {
+      open(sec[i], tab[i]);
     });
   });
-})
+  $.each(cl, function (i, v) {
+    $(v).click(function () {
+      close();
+    });
+  });
+});
--- js/tabToggle.js
+++ js/tabToggle.js
@@ -1,35 +1,33 @@
 var pagination = document.getElementById("pagination");
 var tabs = pagination.getElementsByClassName("tab");
 for (var i = 0; i < tabs.length; i++) {
-  tabs[i].addEventListener("click", function() {
+  tabs[i].addEventListener("click", function () {
     var curr = document.getElementsByClassName("active");
     curr[0].className = curr[0].className.replace(" active", "");
     this.className += " active";
   });
 }
 
-function openTab(section){
-    $(section).slideDown(1000);
-    $('div').animate({
-        scrollTop: 0
-      }, 800);
+function openTab(section) {
+  $(section).fadeIn();
+  $("div").scrollTop(0);
 }
 
-function closeTab(){
-    var ids = ["#dt1", "#dt2", "#dt3", "#dt4"];
-    $.each(ids, function(i, v) {
-        $(v).slideUp(1000);
-    });
+function closeTab() {
+  var ids = ["#dt1", "#dt2", "#dt3", "#dt4"];
+  $.each(ids, function (i, v) {
+    $(v).hide();
+  });
 }
 
-$(document).ready(function(){
-    var tabs = ["#t1", "#t2", "#t3", "#t4"];
-    var ids = ["#dt1", "#dt2", "#dt3", "#dt4"];
-    $.each(ids, function(i, v) {
-        $(tabs[i]).click(function() {
-            closeTab();
-            openTab(v);
-            return false;
-        });
+$(document).ready(function () {
+  var tabs = ["#t1", "#t2", "#t3", "#t4"];
+  var ids = ["#dt1", "#dt2", "#dt3", "#dt4"];
+  $.each(ids, function (i, v) {
+    $(tabs[i]).click(function () {
+      closeTab();
+      openTab(v);
+      return false;
     });
+  });
 });
