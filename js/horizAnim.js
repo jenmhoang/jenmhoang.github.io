b2820d6609ce8b8d1bbc8011b0e2c842bf57d4d0
jenmhoang <jenhoang@berkeley.edu>
2020-05-29 18:04:40 -0700

    hover images

--- js/horizAnim.js
+++ js/horizAnim.js
@@ -1,36 +1,35 @@
 $(document).ready(function () {
-  function open(tag) {
-    $("#frame").attr("src", $(tag).attr("src"));
-    $(".pic").animate({ right: "70%" }, 1000);
-    $(".projNav").fadeOut();
-    $(".info").css("right", "0");
-    $(".info").fadeIn(2000);
-    $(".header").fadeOut();
-    $(".close").fadeIn(2000);
-    $(".close").css("display", "flex");
-  }
-
-  function close() {
-    $(".info").animate({ right: "-70%" }, { duration: 500, queue: false });
-    $(".pic").animate({ right: "0" }, { duration: 500, queue: false });
-
-    $(".projNav").fadeIn(500);
-    $(".info").hide({ duration: 0, queue: true });
-
-    $(".close").hide();
-    $(".header").fadeIn(2000);
-  }
-
-  var btns = ["#b1", "#b2", "#b3", "#b4"];
-  var cl = [".pic", ".close", ".return"];
-  $.each(btns, function (i, v) {
-    $(v).click(function () {
-      open(this);
-    });
+  $(".button").click(function () {
+    open(this);
   });
-  $.each(cl, function (i, v) {
-    $(v).click(function () {
-      close();
-    });
+  $(".button").hover(function () {
+    togglePic(this);
   });
+  $(".close, .pic").on("click", close);
 });
+
+function togglePic(tag) {
+  $(".pic").css("background-image", "url(" + $(tag).attr("href") + ")");
+}
+
+function open(tag) {
+  $("#frame").attr("src", $(tag).attr("src"));
+  $(".pic").animate({ right: "70%" }, 1000);
+  $(".projNav").fadeOut();
+  $(".info").css("right", "0");
+  $(".info").fadeIn(2000);
+  $(".header").fadeOut();
+  $(".close").fadeIn(2000);
+  $(".close").css("display", "flex");
+}
+
+function close() {
+  $(".info").animate({ right: "-70%" }, { duration: 500, queue: false });
+  $(".pic").animate({ right: "0" }, { duration: 500, queue: false });
+
+  $(".projNav").fadeIn({ duration: 500, queue: false });
+  $(".header").fadeIn({ duration: 500, queue: false });
+  $(".info").hide(0);
+
+  $(".close").hide();
+}
